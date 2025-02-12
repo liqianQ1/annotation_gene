cb682f37608b6b38c7d9df35c5ef1d4fbbd77117
liqianQ1 <164116030+liqianQ1@users.noreply.github.com>
2025-02-12 10:42:49 +0800

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,7 @@
+python /pipline/annotation_gene/ann_gene.py \
+        --Outputdir ath1 \
+        --Genome tab/genome.tab \
+        --Homolog tab/homolog.tab \
+        --RNAseq tab/RNAseq.tab \
+        --EST tab/EST.tab \
+        --config /pipline/annotation_gene/config/config.txt
